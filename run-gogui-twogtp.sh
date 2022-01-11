70f43f361934a273bdef03ef541ee065859333e6
WWW <weng0989969828@gmail.com>
2022-01-11 11:18:10 +0800

    change filename

--- run-gogui-twogtp.sh
+++ run-gogui-twogtp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "GoGui-TwoGTP Launcher V20211112"
 # commands for player 1
-P1B='./nogo --shell --name="Hollow-Black" --black="mcts T=1000"'
-P1W='./nogo --shell --name="Hollow-White" --white="mcts T=1000"'
+P1B='./WWW --shell --name="Black" --black="mcts N=700 T=1000"'
+P1W='./WWW --shell --name="White" --white="mcts N=700 T=1000"'
 # commands for local player 2
 P2B='./nogo-judge --shell --name="Judge-Weak-Black" --black="weak"'
 P2W='./nogo-judge --shell --name="Judge-Weak-White" --white="weak"'
